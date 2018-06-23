4367c7a86cf0f79a25418caa31e477ea847c2b88
richard.c.chen@gmail.com <richard.c.chen@gmail.com>
2018-06-23 23:10:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function dec(n){
 }
 
 function makeInt(n){
-  n === parseInt('n',10)
+  n === parseInt('n', 10)
   return n
 }
 
 function preserveDecimal(n){
-  parseFloat('n')
+  n === parseFloat('n')
   return n
 }
